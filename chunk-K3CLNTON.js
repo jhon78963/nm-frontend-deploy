f9b33704e39820d919b73071844a8342fbec0a51
Jhon Livias <jhonlivias3@gmail.com>
2026-05-22 13:11:05 -0500

    fixing

--- chunk-K3CLNTON.js
+++
@@ -1,1 +0,0 @@
-import{Ra as e,V as r}from"./chunk-2PVWE257.js";var c=(()=>{class t{constructor(){this.snapshot=e(null)}read(){return this.snapshot()}save(s){this.snapshot.set(s)}clear(){this.snapshot.set(null)}static{this.\u0275fac=function(n){return new(n||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{c as a};
